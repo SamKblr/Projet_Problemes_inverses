e53becc5cbb97bb236e47fd460dcdb2e791a1a84
Kubler Samuel <skubler@localhost.localdomain>
2018-12-28 15:50:50 +0100

    Ajouts de fichiers + début question 3

--- main_demo.m
+++ main_demo.m
@@ -4,20 +4,20 @@ clear all
 
 %% Simulation of a gaussian law truncated to positive values
 % Var
-sigma2 = 2;
+sigma2 = 3;
 m = 4;
-N = 1e6;
+N = 1e5;
 t = linspace(0,10);
 
-
+% Questions 1-2
 X_test = randnt_inversion(m, sigma2, N);
-
-
 K = sqrt(pi*sigma2/2)*(1+erf(m/sqrt(sigma2*2)));
-
 h = histogram(X_test,100);
-
 hmax = max(h.Values);
+
+% Question 3
+
+
 
 %% Affichage ex2
 
---
+++ randnt_ar1.m
@@ -0,0 +1,6 @@
+function X = randnt_ar1(m,sigma2,N)
+
+u = rand(N,1);
+
+
+end
